ad57f2e811ce5097462342eb503b5aed43dd9afc
khchoi <khchoi0301@gamil.com>
2018-09-19 16:02:16 +0900

    remove func name

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,27 +1,27 @@
 /* global _ */
-var Stack = function () {
-  var someInstance = {}
-  someInstance.count = 0
-  someInstance.storage = {}
-  _.extend(someInstance, stackMethods)
-  return someInstance
-}
-
 var stackMethods = {}
 stackMethods.push = function (value) {
   this.storage[this.count] = value
-  this.count++
+  this.count += 1
 }
 stackMethods.pop = function () {
   if (this.count > 0) {
-    this.count--
-    let temp = this.storage[this.count]
+    this.count -= 1
+    var temp = this.storage[this.count]
     delete this.storage[this.count]
     return temp
   }
 }
 stackMethods.size = function () {
   return this.count
+}
+
+var Stack = function () {
+  var someInstance = {}
+  someInstance.count = 0
+  someInstance.storage = {}
+  _.extend(someInstance, stackMethods)
+  return someInstance
 }
 
 if (typeof module === 'object' && typeof module.exports === 'object') {

bb7670c798ada4bee41d70db5e9c606c2b45ba46
khchoi <khchoi0301@gamil.com>
2018-09-19 12:12:20 +0900

    complete eslint

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,24 +1,36 @@
-var Queue = function() {
-  var someInstance = {};
+var Queue = function () {
+  var someInstance = {}
 
   // Use an object with numeric keys to store values
-  var storage = {};
-
+  var storage = {}
+  var count = 0
   // Implement the methods below
 
-  someInstance.enqueue = function(value) {
-  };
+  someInstance.enqueue = function (value) {
+    storage[count] = value
+    count++
+  }
 
-  someInstance.dequeue = function() {
-  };
+  someInstance.dequeue = function () {
+    if (count > 0) {
+      let temp = storage[0]
+      delete storage[0]
+      for (var key in storage) {
+        storage[key - 1] = storage[key]
+      }
+      count--
+      return temp
+    }
+  }
 
-  someInstance.size = function() {
-  };
+  someInstance.size = function () {
+    return count
+  }
 
-  return someInstance;
-};
+  return someInstance
+}
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
+if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = {
     Queue
   }

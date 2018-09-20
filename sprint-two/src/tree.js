eb2ab8b6a780ffdce9e6ac2d8f59ba59cc469f94
khchoi <khchoi0301@gamil.com>
2018-09-20 14:37:46 +0900

    complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,27 +1,56 @@
-var Tree = function(value) {
-  var newTree = {};
-  newTree.value = value;
+var Tree = function (value) {
+  var newTree = Object.create(treeMethods)
+  newTree.value = value
+  newTree.children = []
+  return newTree
+}
 
-  // your code here
-  newTree.children = null;  // fix me
+var treeMethods = {}
 
-  return newTree;
-};
+treeMethods.addChild = function (value) {
+  var a = new Tree(value)
+  this.children.push(a)
+}
 
-var treeMethods = {};
-
-treeMethods.addChild = function(value) {
-};
-
-treeMethods.contains = function(target) {
-};
-
-
+treeMethods.contains = function (target) {
+  let result = false
+  function check (obj) {
+    if (obj.value === target) {
+      result = true
+    } else {
+      for (let i = 0; i < obj.children.length; i++) {
+        check(obj.children[i])
+      }
+    }
+  }
+  check(this)
+  return result
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-if ( typeof module === "object" && typeof module.exports === "object" ) {
-  module.exports = Tree;
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = Tree
 }
+
+// treeMethods.contains = function(target) {
+//   console.log(this)
+//   var result;
+//   var test = function check (obj){
+//     console.log(obj,target,obj.value)
+//     if(obj.value === target) {
+//       return true
+//     } else {
+//       for(let i = 0 ; i < obj.children.length ; i++){
+//         console.log(i,'child',obj.children[i])
+//         check(obj.children[i])
+//       }
+//     }
+//   }
+
+//   console.log(test(this)===undefined ? false : true)
+//   return test(this)===undefined ? false : true
+
+// };

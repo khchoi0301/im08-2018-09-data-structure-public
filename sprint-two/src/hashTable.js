ca00ddfc4aef1dce408830d69c5cb2bea9bae85c
khchoi <khchoi0301@gamil.com>
2018-09-21 15:01:17 +0900

    complete hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,31 +1,55 @@
-if ( typeof module === "object" && typeof module.exports === "object" ) {
-  var { LimitedArray, getIndexBelowMaxForKey } = require('./hashTableHelpers');
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  var { LimitedArray, getIndexBelowMaxForKey } = require('./hashTableHelpers')
 }
 
-var HashTable = function() {
-  this._limit = 8;
-  this._storage = LimitedArray(this._limit);
-};
+var HashTable = function () {
+  this._limit = 8
+  this._storage = LimitedArray(this._limit)
+}
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-};
+HashTable.prototype.insert = function (k, v) {
+  var index = getIndexBelowMaxForKey(k, this._limit)
+  let tempArr = []
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  let findKey = function (that) {
+    if (that._storage.get(index) !== undefined) {
+      for (let i = 0; i < that._storage.get(index).length; i++) {
+        tempArr.push(that._storage.get(index)[i])
+        if (that._storage.get(index)[i] === k) {
+          return true
+        }
+      }
+      return false
+    }
+  }
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-};
+  if (this._storage.get(index) === undefined || findKey(this)) {
+    this._storage.set(index, [k, v])
+  } else {
+    tempArr.push(k, v)
+    this._storage.set(index, tempArr)
+  }
+}
 
+HashTable.prototype.retrieve = function (k) {
+  var index = getIndexBelowMaxForKey(k, this._limit)
+  // console.log(this._storage.get(index), k)
+  for (let i = 0; i < this._storage.get(index).length; i++) {
+    if (this._storage.get(index)[i] === k) {
+      return this._storage.get(index)[i + 1]
+    }
+  }
+}
 
+HashTable.prototype.remove = function (k) {
+  var index = getIndexBelowMaxForKey(k, this._limit)
+  this._storage.set(index, [])
+}
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-
-if ( typeof module === "object" && typeof module.exports === "object" ) {
-  module.exports = HashTable;
+if (typeof module === 'object' && typeof module.exports === 'object') {
+  module.exports = HashTable
 }
